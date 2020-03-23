45dbd2564ca0a6ef035a8ecfb054f0e293ace9cd
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-03-23 14:08:13 +0100

    Commit Josema Inicio Login

--- app/src/main/java/com/example/proyectointegradojmjq/Login.java
+++ app/src/main/java/com/example/proyectointegradojmjq/Login.java
@@ -2,13 +2,32 @@ package com.example.proyectointegradojmjq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnRegistrarLogin, btnEntrarLogin, btnRecordarClave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegistrarLogin = findViewById(R.id.btnRegistrarLogin);
+
+        btnRegistrarLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btnRegistrarLogin:
+                Intent intentRegistrar = new Intent(Login.this, CrearUsuario.class);
+                startActivity(intentRegistrar);
+        }
     }
 }
