ee6b70830e83fbaf5e3ffa09da27ebdd72bf6c58
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-03-24 11:38:35 +0100

    Commit Josema Cambios de diseño y pantalla recordar contraseña

--- app/src/main/java/com/example/proyectointegradojmjq/Login.java
+++ app/src/main/java/com/example/proyectointegradojmjq/Login.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnRegistrarLogin, btnEntrarLogin, btnRecordarClave;
+    Button btnRegistrarLogin, btnEntrarLogin;
+    TextView lblRecordarClave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,10 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.activity_login);
 
         btnRegistrarLogin = findViewById(R.id.btnRegistrarLogin);
+        lblRecordarClave = findViewById(R.id.lblRecordarClave);
 
         btnRegistrarLogin.setOnClickListener(this);
+        lblRecordarClave.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +32,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
             case R.id.btnRegistrarLogin:
                 Intent intentRegistrar = new Intent(Login.this, CrearUsuario.class);
                 startActivity(intentRegistrar);
+                break;
+
+            case R.id.lblRecordarClave:
+                Intent intentRecordarClave = new Intent(Login.this, RecordarClave.class);
+                startActivity(intentRecordarClave);
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/example/proyectointegradojmjq/RecordarClave.java
@@ -0,0 +1,14 @@
+package com.example.proyectointegradojmjq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordarClave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordar_clave);
+    }
+}
