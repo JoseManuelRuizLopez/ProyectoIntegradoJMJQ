e02b95c0a6efac4c03d49809c0c22361213357eb
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2020-03-25 15:16:45 +0100

    Commit Josema sharedpreferences del login y del inicio de la app

--- app/src/main/java/com/example/proyectointegradojmjq/MainActivity.java
+++ app/src/main/java/com/example/proyectointegradojmjq/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.proyectointegradojmjq;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView logoApp;
     TextView txt;
 
+    SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         txt = findViewById(R.id.lblLogoApp);
         txt.setOnClickListener(this);
+
+        sharedPref = getSharedPreferences("logeado", Context.MODE_PRIVATE);
     }
 
     @Override
@@ -32,10 +40,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId())
         {
             case R.id.logoApp:
-                Intent intentLogin = new Intent(MainActivity.this, Login.class);
-                startActivity(intentLogin);
-                finish();
-                break;
+
+                Boolean logeado = sharedPref.getBoolean("isLogged", false);
+
+                if(logeado == true)
+                {
+                    Intent intentLogin = new Intent(MainActivity.this, MenuPrincipal.class);
+                    startActivity(intentLogin);
+                    finish();
+                    break;
+                }
+                else
+                {
+                    Intent intentLogin = new Intent(MainActivity.this, Login.class);
+                    startActivity(intentLogin);
+                    finish();
+                    break;
+                }
 
             case R.id.lblLogoApp:
                 Intent intentNuevo = new Intent(MainActivity.this, CrearUsuario.class);
